e990983471458192f7a4f2cea9e42c5a8ff4215b
Dmitrii Abdukadyrov <abdukadyrov.dv@gmail.com>
2022-09-16 00:09:49 +0100

    remove api key from example

--- examples/solveCoordinatesCaptcha.js
+++ examples/solveCoordinatesCaptcha.js
@@ -4,7 +4,7 @@ const {CoordinatesCaptcha} = require("../src/CoordinatesCaptcha");
 
 (async function () {
     const captchaImage = await fs.readFileSync(__dirname + '/coordinates.png');
-    const solver = new Solver('a075dd3abbf8b75b3f88d7368a96091c');
+    const solver = new Solver('<your api key>');
     const captcha = new CoordinatesCaptcha();
     captcha.method = 'base64';
     captcha.body = captchaImage.toString('base64');
